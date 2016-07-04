e0ca2afef14192d8d2460ca70afc4b08336b73ed
Sergio Castro R <sergio.castro@sapiensdev.com>
2016-07-04 11:04:30 -0600

    Fixing error

--- db/migrate/20160629153638_change_column_permission_levl_from_string_to_integer.rb
+++ db/migrate/20160629153638_change_column_permission_levl_from_string_to_integer.rb
@@ -1,6 +1,6 @@
 class ChangeColumnPermissionLevlFromStringToInteger < ActiveRecord::Migration
   def change
     remove_column :users,:permission_level,:string
-    add_column :users,:permission_level,:integer, default: 1
+    add_column :users,:permission_level,:integer, default: 3
   end
 end
